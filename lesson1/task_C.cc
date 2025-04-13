4c26e876b98100946b4a3ecf57a8a272c984c8fb
wendolyr <a_sekhlyan@DESKTOP-RH2NDB4>
2025-04-13 23:19:40 +0300

    lesson1 md taskA

---
+++ lesson1/task_C.cc
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#define TYPE 30
+using namespace std;
+
+int main() {
+  long long m = 0;
+  cin >> m;
+
+  vector<long long> a(31);
+  for (int i = 0; i <= TYPE; i++) {
+    cin >> a[i];
+  }
+
+    return 0;
+}
