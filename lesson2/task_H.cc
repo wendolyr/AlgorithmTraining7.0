05379b3e11643c4ad527d203becf828358a435d7
Wendolyn Reyne <wendolyr@si-k5.kzn.21-school.ru>
2025-04-19 15:54:02 +0300

    lesson 2 task H

---
+++ lesson2/task_H.cc
@@ -0,0 +1,128 @@
+#include <climits>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Segment_tree {
+ private:
+  int size;
+  vector<long long> values;
+  vector<long long> promises;
+
+ public:
+  Segment_tree(const vector<int>& n) {
+    size = 1;
+    while (size < (int)n.size()) {
+      size <<= 1;
+    }
+    size = 2 * size - 1;
+    values.resize(size, 0);
+    promises.resize(size, 0);
+
+    int middle = size / 2;
+    for (size_t i = middle; i < middle + n.size(); i++) {
+      values[i] = n[i - middle];
+    }
+
+    for (int i = middle + n.size(); i < size; i++) {
+      values[i] = 0;
+    }
+
+    for (int i = middle - 1; i >= 0; i--) {
+      int l_child = 2 * i + 1;
+      int r_child = 2 * i + 2;
+      values[i] = max(values[l_child], values[r_child]);
+    }
+  }
+
+  void add_to_data(int value, int left, int right) {
+    left--;
+    right--;
+    add_to_data(value, left + size / 2, right + size / 2, 0, size / 2,
+                size - 1);
+  }
+
+  void add_to_data(int value, int left, int right, int node, int node_l,
+                   int node_r) {
+    if (node_l > right || node_r < left) {
+      return;
+    }
+    if (node_l >= left && node_r <= right) {
+      values[node] += value;
+      promises[node] += value;
+      return;
+    }
+
+    add_to_data(value, left, right, 2 * node + 1, node_l,
+                (node_l + node_r) / 2);
+    add_to_data(value, left, right, 2 * node + 2, (node_l + node_r) / 2 + 1,
+                node_r);
+
+    values[node] = max(values[2 * node + 1], values[2 * node + 2]);
+  }
+
+  long long find_elem(int index) {
+    index--;
+
+    return find_elem(size / 2 + index, 0, size / 2, size - 1);
+  }
+
+  long long find_elem(int find_index, int node, int node_l, int node_r) {
+    if (node_l > find_index || node_r < find_index) {
+      return -1;
+    }
+    if (node == find_index) {
+      return values[node];
+    }
+
+    if (promises[node]) {
+      promises[2 * node + 1] += promises[node];
+      promises[2 * node + 2] += promises[node];
+      values[2 * node + 1] += promises[node];
+      values[2 * node + 2] += promises[node];
+      promises[node] = 0;
+    }
+
+    long long result = 0;
+    int middle = (node_l + node_r) / 2;
+    if (find_index <= middle)
+      result = find_elem(find_index, 2 * node + 1, node_l, middle);
+    else {
+      result = find_elem(find_index, 2 * node + 2, middle + 1, node_r);
+    }
+
+    return result;
+  }
+};
+
+int main() {
+  int n = 0, k = 0;
+
+  cin >> n;
+  vector<int> numbers(n);
+  for (int i = 0; i < n; i++) {
+    cin >> numbers[i];
+  }
+
+  Segment_tree data(numbers);
+
+  cin >> k;
+
+  for (int i = 0; i < k; i++) {
+    char command = ' ';
+    cin >> command;
+    if (command == 'g') {
+      int position = 0;
+      cin >> position;
+      cout << data.find_elem(position) << endl;
+    } else if (command == 'a') {
+      pair<int, int> range;
+      int add = 0;
+      cin >> range.first >> range.second >> add;
+      data.add_to_data(add, range.first, range.second);
+    }
+  }
+
+  return 0;
+}
