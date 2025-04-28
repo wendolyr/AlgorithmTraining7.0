ed94547840a132f0cd7c4560aa72f3049db7a28e
Wendolyn Reyne <wendolyr@si-j4.kzn.21-school.ru>
2025-04-28 18:32:00 +0300

    lesson 3 task I

--- lesson3/task_C.cc
+++ lesson3/task_C.cc
@@ -27,5 +27,73 @@ int main() {
     max >>= 1;
   }
 
+  int res = a[0];
+  for (int i = 1; i < n; i++) {
+    res ^= a[i];
+  }
+
+  if (res == 0) {
+    for (auto x : a) {
+      cout << x << ' ';
+    }
+    return 0;
+  } else {
+    unsigned long long min = a[0];
+    for (int i = 1; i < n; i++) {
+      if (a[i] < min) {
+        min = a[i];
+      }
+    }
+    if (min == 1) {
+      cout << "impossible";
+      return 0;
+    }
+  }
+
+  for (int i = 0; i < max_bits; i++) {
+    int sum_bit = 0;
+    for (int j = 0; j < n; j++) {
+      if (a[j] & (1 << i)) {
+        sum_bit++;
+      }
+    }
+
+    if (sum_bit % 2 == 1) {
+      int finder = -1;
+      int new_sum = 0;
+      for (int j = i + 1; j < max_bits; j++) {
+        for (int k = 0; k < n; k++) {
+          if (a[k] & (1 << j)) {
+            new_sum++;
+          }
+        }
+        if (new_sum % 2 == 1) {
+          finder = j;
+          break;
+        }
+      }
+      if (finder == -1) {
+        cout << "impossible";
+        return 0;
+      } else {
+        for (int j = 0; j < n; j++) {
+          if ((a[j] & (1 << i)) && !(a[j] & (1 << finder))) {
+            a[j] &= ~(1ULL << i);
+            a[j] |= 1ULL << finder;
+            break;
+          } else if (!(a[j] & (1 << i)) && (a[j] & (1 << finder))) {
+            a[j] &= ~(1ULL << finder);
+            a[j] |= 1ULL << i;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  for (auto x : a) {
+    cout << x << ' ';
+  }
+
   return 0;
 }
--- lesson3/task_I.cc
+++ lesson3/task_I.cc
@@ -1,42 +1,79 @@
+#include <cmath>
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-void sendPack() {
-  string pack;
-  cin >> pack;
-
-  for (char c : pack) {
-    cout << c << c << c;
+string encode(const string &x) {
+  int n = x.size();
+  int k = 0;
+  while ((1 << k) < n + k + 1) k++;
+  string y(n + k, '0');
+  int j = 0;
+  for (int i = 1; i <= n + k; i++) {
+    if ((i & (i - 1)) != 0) {
+      y[i - 1] = x[j++];
+    }
   }
+
+  for (int i = 0; i < k; i++) {
+    int control_bit = (1 << i);
+    int counter = 0;
+    for (int q = control_bit + 1; q <= n + k; q++) {
+      if ((control_bit & q) != 0 && y[q - 1] == '1') {
+        counter++;
+      }
+    }
+    y[control_bit - 1] = counter % 2 == 0 ? '0' : '1';
+  }
+
+  return y;
 }
 
-void getPack() {
-  string pack;
-  cin >> pack;
-  int final_bit = 0;
-  int counter = 0;
-  for (size_t i = 0; i < pack.size(); i++) {
-    if (pack[i] == '1') {
-      final_bit += 1;
+string decode(const string &z) {
+  int n = z.size();
+  int k = 0;
+  while ((1 << k) < n + 1) k++;
+
+  int error_pos = 0;
+  for (int i = 0; i < k; i++) {
+    int control_bit = (1 << i);
+    int counter = 0;
+    for (int q = control_bit + 1; q <= n + k; q++) {
+      if ((control_bit & q) != 0 && z[q - 1] == '1') {
+        counter++;
+      }
     }
-    counter++;
-    if (counter == 3) {
-      cout << final_bit / 2;
-      counter = 0;
-      final_bit = 0;
+    char check = counter % 2 == 0 ? '0' : '1';
+    if (z[control_bit - 1] != check) {
+      error_pos += control_bit;
     }
   }
+
+  string y = z;
+  if (error_pos > 0 && error_pos <= n) {
+    y[error_pos - 1] = (y[error_pos - 1] == '1' ? '0' : '1');
+  }
+
+  string x;
+  for (int i = 0; i <= n; i++) {
+    if ((i & (i - 1)) != 0) {
+      x += y[i - 1];
+    }
+  }
+  return x;
 }
 
 int main() {
-  int option = 0;
-  cin >> option;
-  if (option == 1) {
-    sendPack();
-  } else if (option == 2) {
-    getPack();
-  }
-
+  int run;
+  cin >> run;
+  cin.ignore();
+  string data;
+  getline(cin, data);
+  if (run == 1)
+    cout << encode(data) << endl;
+  else if (run == 2)
+    cout << decode(data) << endl;
   return 0;
 }
